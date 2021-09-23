60ef0b0815d127826987983e5f7eb753875aa801
suellen <adnarim.suellen@gmail.com>
2021-09-23 18:15:05 -0300

    Adicionar arquivos de projeto.

---
+++ Condicional/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando adicionais!");
+            Console.WriteLine();
+
+            int idadeSuellen = 11;
+            int NumDeAcompanhantes = 1;
+
+            if (idadeSuellen >= 18)
+
+
+            {
+
+                Console.WriteLine("Legal! Você é maior de 18 anos e pode entrar no cinema!");
+                Console.WriteLine("Seja bem-vindx");
+
+            }
+
+            else
+
+            {
+                if (NumDeAcompanhantes >= 2)
+
+                {
+                    Console.WriteLine("Você não tem mais de 18 anos, mas pode entrar acompanhandx!");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Infelizmente você é menor de 18 anos e poderá entrar somente acompanhadx!");
+                }
+            }
+        }
+    }
+}
